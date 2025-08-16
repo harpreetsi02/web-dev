2495b58f8f6ea516c4c35ed8b9e4bc7de527d80c
Apple <apple@Apples-MacBook-Pro.local>
2025-08-16 18:19:04 +0530

    changes

---
+++ ForJsPractice/practice js/JsClass1.js
@@ -0,0 +1,104 @@
+// // Print hello
+// // console.log("hello")
+
+
+
+// // Simple Addition
+// // let a = 5;
+// // let b = 10;
+// // console.log("Sum: ", a + b);
+
+
+
+// // User Input
+// // let name = prompt("Enter your name");
+// // alert("Hello, " + name + "!");
+
+
+
+// // Even or Odd
+// // let num = prompt("Enter a Number: ");
+// // if(num % 2 === 0){
+// //     console.log("This is Even number");
+// // }
+// // else{
+// //     console.log("This is Odd number");
+// // }
+
+
+
+// // Multiplication Table
+// // let n = 5;
+// // for (let i = 1; i <= 10; i++){
+// //     console.log(`${n} x ${i} = ${n * i}`);
+// // }
+
+
+
+
+// // // Simple Calculation
+// // let a = 28, b = 12;
+// // console.log("Addition: ", a + b);
+// // console.log("substract: ", a - b);
+// // console.log("Divide: ", a/b);
+// // console.log("Multiplication: ", a * b);
+
+
+
+// // // find largest number
+// // let a1 = 12, b1 = 13;
+// // if (a1 > b1) {
+// //     console.log(a + " is larger")
+// // }
+// // else{
+// //     console.log(b + " is larger")
+// // }
+
+
+
+// // Reverse a String
+// let str = "JavaScript";
+// let reversed = str.split("") .reverse() .join("");
+// console.log(reversed);
+
+
+
+// // Factorial number
+// let num1 = 5;
+// let fact = 1;
+// for (let i = 1; i <= num1; i++){
+//     fact *= i;
+// }
+// console.log("Factorial:", fact);
+
+
+
+// // Leep year
+// let year = 2008;
+// if ((year % 4 === 0 && year % 100 !== 0 || year % 400 === 0)) {
+//     console.log(year +" This is a leep year")
+// } 
+// else {
+//     console.log("This is not leep year")
+// }
+
+
+// // print 1 to 10
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+
+// // String length
+// let str1 = "JavaScript Rocks!";
+// console.log(str1.length)
+
+
+
+// Simple While loop
+let i = 1;
+while (i <= 5) {
+    console.log("Count: ", i);
+    i++;
+}
---
+++ ForJsPractice/practice js/practice.js
@@ -0,0 +1,10 @@
+// you are aligible or not for vote
+
+let name = prompt("Enter Your name")
+let age = prompt("Enter your age")
+if (age >= 18) {
+    console.log(`${name} You are aligible for vote`)
+
+} else {
+    console.log(`${name} You are not aligible for vote`)
+}
